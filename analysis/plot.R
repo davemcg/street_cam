0e0b6da02fc0f9c213843658423c610a3d271454
David McGaughey <mcgaughey@gmail.com>
2018-07-25 23:12:36 -0400

    produces plot of car / ped per hour facet by day

--- analysis/plot.R
+++ analysis/plot.R
@@ -13,7 +13,21 @@ data <- read_csv('/Volumes/share/pi/speed-camera/speed-cam.csv',
 # if within 1 second, group together
 data$incident <- cumsum(c(1, diff(data$HMSS)) >= 10)
 
-data %>% tail()
-
-data %>% filter(Speed < 30) %>% group_by(Date, incident) %>% summarise(Speed = mean(Speed), Time = mean(Time)) %>% 
-  ggplot(aes(x=Time, y=Speed)) + facet_wrap(Date) + geom_point()
+data %>% 
+  filter(Speed < 30) %>% 
+  group_by(Date, incident) %>% 
+  summarise(Speed = mean(Speed), 
+            Time = mean(Time),
+            Hour = min(Hour),
+            Direction = names(which.max(table(Direction)))) %>%
+  mutate(Direction = case_when(Direction == 'L2R' ~ 'Westward',
+                               TRUE ~ 'Eastward')) %>% 
+  ungroup() %>% 
+  group_by(Date, Hour, Direction) %>% 
+  summarise(Count = n()) %>% 
+  ggplot(aes(x=Hour, y=Count, colour = Direction)) + 
+  facet_wrap(~Date) +
+  coord_cartesian(ylim=c(0,65)) +
+  geom_step() +
+  theme_minimal() +
+  ggsci::scale_color_lancet()
